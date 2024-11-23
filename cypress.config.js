07e54af0a406986a034d18f229946180b8ff377d
bismalmajeed <77693261+bismalmajeed@users.noreply.github.com>
2024-11-24 00:17:17 +0500

    cypress Parallel fix

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "ocnw79",
+  video: false,
+  reporter: "cypress-mochawesome-reporter",
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 
